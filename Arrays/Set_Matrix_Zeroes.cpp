cee4c5342dbe6e0b18ff4207edc31d6742160cf0
pajju <prajwalshah.2003@gmail.com>
2023-06-07 09:08:51 +0530

    firstr commit

---
+++ Arrays/Set_Matrix_Zeroes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int>rows;
+        vector<int>cols;
+        for(int i = 0; i < matrix.size(); ++i){
+            for(int j = 0; j < matrix[0].size(); ++j){
+                if(matrix[i][j] == 0){
+                    rows.push_back(i);
+                    cols.push_back(j);
+                }
+            }
+        }
+        for(auto &j: rows){
+            for(int i = 0; i < matrix[0].size(); ++i){
+                matrix[j][i] = 0;
+            }
+        }
+        for(auto &j: cols){
+            for(int i = 0; i < matrix.size(); ++i){
+                matrix[i][j] = 0;
+            }
+        }
+
+    }
+};
